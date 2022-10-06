b51c5439e59cb7b9258fbe8316b665e67bf79fa8
Nadzeya <lustra2013@gmail.com>
2022-10-06 10:09:15 +0300

    seminar2

---
+++ seminar2/cs5.1/Program.cs
@@ -0,0 +1,16 @@
+﻿// программа, которая принимает на вход трехзначное число
+// и на выходе показывает вторую цифру этого числа
+Console.WriteLine("Введите трехзначное число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int num1 = x / 10;
+int num2 = num1 % 10;
+
+if(num1 / 10 > 1 && num1 / 10 <10 )
+{
+     Console.WriteLine($"средняя цифра:{num2}");
+}
+else
+{
+    Console.WriteLine("ошибка ввода"); 
+}
+
