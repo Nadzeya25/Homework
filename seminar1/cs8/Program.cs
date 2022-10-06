b6e230ab0e1cc4a078af7e0d4d035c8378d3d51d
Nadzeya <lustra2013@gmail.com>
2022-10-06 10:08:57 +0300

    seminar1

---
+++ seminar1/cs8/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+Console.Write("N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+int num = 2;
+while(num <= N)
+{
+    Console.WriteLine($"{num}");
+    num = num + 2;
+}
