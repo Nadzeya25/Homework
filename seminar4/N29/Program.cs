b2a9664ef9b8d155cb72ebdc6166e4a7f8ba7105
Nadzeya <lustra2013@gmail.com>
2022-10-17 18:10:48 +0300

    add correct2 hw 4

--- seminar4/N29/Program.cs
+++ seminar4/N29/Program.cs
@@ -1,34 +1,37 @@
 ﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
-void FillArray(int[] box) 
-{
-    
-    int size = box.Length; 
-    int index = 0;
-    while (index < size)  
-    {
-        box[index] = new Random().Next(0, 100);        
-        index++;                                     
-        
-    }
-}
-
-void PrinrArray(int[] box)   
+void FillArray(int[] box)
 {
     int size = box.Length;
     int index = 0;
     while (index < size)
     {
-        Console.Write($"{box[index]} "); 
+        box[index] = new Random().Next(-20, 20);
+        index++;
+
+    }
+}
+
+void PrinrArray(int[] box)
+{
+    int size = box.Length;
+    int index = 0;
+    while (index < size)
+    {
+        if (index < size - 1)
+            Console.Write($"{box[index]}, ");
+        else
+            Console.WriteLine($"{box[index]} ]\n");
+
         index++;
     }
 }
 int[] box = new int[8];   // задаем размер массива 8 элементов
-FillArray(box);   
+FillArray(box);
 Console.Write("[");
-PrinrArray(box);  
-Console.Write("]");
+PrinrArray(box);
+
 
 
 
