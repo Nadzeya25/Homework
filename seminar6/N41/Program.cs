7cf5167b67e5942162d945d23fd79138386b37d9
Nadzeya <lustra2013@gmail.com>
2022-10-20 04:01:43 +0300

    add seminar6

---
+++ seminar6/N41/Program.cs
@@ -0,0 +1,48 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел.
+ Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+Console.Write("Введите, какое количество значений нужно проверить: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[length];
+int count = 0;
+Console.WriteLine("Вводите числа поочередно, подтверждая ввод каждого числа кнопкой ENTER");
+
+void FillArray(int[] box)
+{
+    int size = box.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        
+        box[i] = Convert.ToInt32(Console.ReadLine());
+        if (box[i] > 0)
+        {
+            count++;
+        }
+        
+    }
+    
+}
+
+void PrintArray(int[] box)
+{
+    int length = box.Length;
+    int i = 0;
+    while (i < length)
+    {
+        if (i < length - 1)
+            Console.Write($"{box[i]}, ");
+        else
+            Console.WriteLine($"{box[i]} ]\n");
+
+        i++;
+    }
+}
+
+
+FillArray(array);
+Console.Write("проверьте введенные числа : [");
+PrintArray(array);
+Console.WriteLine($"количество положительных чисел в последовательности равно: {count}");
